abcd3963ce5a2d154f85e5908cde54f83088583f
David Wilson-Brown <77813686+DavidWilson-Brown@users.noreply.github.com>
2021-11-16 21:23:33 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,106 @@
+'use strict';
+
+const textInput = document.getElementByID('textInput');
+const textOutput = document.getElementByID('textOutPut');
+
+textInput.addEventListener('keyup', (e) => {
+  const input = e.target.value;
+  // translate to pig latin and display new text
+  textOutput.innerText = input;
+});
+// brings in the assert module for unit testing
+const assert = require('assert');
+const { waitForDebugger } = require('inspector');
+// brings in the readline module to access the command line
+const readline = require('readline');
+// use the readline module to print out to the command line
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+const pigLatin = (str) => {
+    // get the reference for the body
+    let body = document.getElementsByTagName("body");
+    // creates an <input> element
+    let input = document.
+    // Convert string to lowercase and trim
+    str = str.toLowerCase().trim();
+    // Initialize array of vowels
+    const vowels = ["a", "e", "i", "o", "u"];
+    // Initialize vowel index to 0
+    let vowelIndex = 0;
+  
+    if (vowels.includes(str[0])) {
+      // If first letter is a vowel
+      return str + "yay";
+    } else {
+      // If the first letter isn't a vowel i.e is a consonant
+      for (let char of str) {
+        // Loop through until the first vowel is found
+        if (vowels.includes(char)) {
+          // Store the index at which the first vowel exists
+          vowelIndex = str.indexOf(char);
+          break;
+        }
+      }
+      // Compose final string
+      return str.slice(vowelIndex) + str.slice(0, vowelIndex) + "ay";
+    }
+  }
+
+// the first function called in the program to get an input from the user
+// to run the function use the command: node main.js
+// to close it ctrl + C
+const getPrompt = () => {
+  rl.question('word ', (answer) => {
+    console.log( pigLatin(answer) );
+    getPrompt();
+  });
+}
+
+// Unit Tests
+// to use them run the command: npm test main.js
+// to close them ctrl + C
+if (typeof describe === 'function') {
+
+  describe('#pigLatin()', () => {
+    it('should translate a simple word', () => {
+      assert.equal(pigLatin('car'), 'arcay');
+      assert.equal(pigLatin('dog'), 'ogday');
+    });
+    it('should translate a complex word', () => {
+      assert.equal(pigLatin('create'), 'eatecray');
+      assert.equal(pigLatin('valley'), 'alleyvay');
+    });
+    it('should attach "yay" if word begins with vowel', () => {
+      assert.equal(pigLatin('egg'), 'eggyay');
+      assert.equal(pigLatin('emission'), 'emissionyay');
+    });
+    it('should lowercase and trim word before translation', () => {
+      assert.equal(pigLatin('HeLlO '), 'ellohay');
+      assert.equal(pigLatin(' RoCkEt'), 'ocketray');
+    });
+  });
+} else {
+
+  getPrompt();
+  
+}
+
+
+
+
+
+
+
+
+// **********
+//   HINTS
+// **********
+
+// break your code into pieces and focus on one piece at a time...
+// 1. if word begins with a vowel send to one function: adds "yay"
+// 2. if word begins with a consonant send to another function: splices off beginning, returns word with new ending.
+// 3. if multiple words, create array of words, loop over them, sending them to different functions and creating a new array with the new words.
